08b170c6a6e50308c42a927ae6b4bd4514fe51c0
ClemAMolette <clementinegaga79@outlook.fr>
2025-03-16 18:30:22 +0100

    select box for categories

--- src/frontend/pages/1_🤖_Chatbot.py
+++ src/frontend/pages/1_🤖_Chatbot.py
@@ -29,24 +29,35 @@ def evaluate_answer(answer):
     st.session_state["good_answers"] += 1
 
 
+# category selection option
+category = st.selectbox(
+    "Select a specific category to train on :",
+    ("All categories", "c1", "c2"),
+)
+st.write("Category selected:", category)
 
-# affichage de l'historique des messages
+st.divider()
+
+
+# display message history
 for message in st.session_state["messages"]:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# récupération du message utilisateur
+# get user message
 if prompt := st.chat_input("Que voulez-vous savoir?"):
     context_prompt = get_context_prompt(prompt)
 
-    # on ajoute le message utilisateur au format {role, content}
+    # adding user message in format {role, content}
     st.session_state["messages"].append({"role": "user", "content": context_prompt})
     # affichage du nouveau message
     with st.chat_message("user"):
         st.markdown(prompt)
     
-    # récupération et affichage réponse modèle
+    # get and display model message
     with st.chat_message("assistant"):
         message = st.write_stream(model_res_generator())
         st.session_state["messages"].append({"role": "assistant", "content": message})
+
+        # test for statistics variables
         evaluate_answer(context_prompt)
