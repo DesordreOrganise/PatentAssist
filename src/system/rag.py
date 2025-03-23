bf9b62c049afd59f21c7011867c247ed984f5dac
abtblank <t9cube@hotmail.fr>
2025-03-23 19:28:19 +0100

    add vectorstore persistence

--- src/system/rag.py
+++ src/system/rag.py
@@ -10,10 +10,15 @@ import torch
 import torch.nn.functional as F
 
 import networkx as nx
-from typing import Tuple
+from typing import Tuple, Optional
 from os import PathLike
-import yaml
+from pathlib import Path
 
+from src.utils.tools import load_config
+
+
+ROOT_DIR = Path(__file__).resolve().parent.parent.parent
+CHROMA_DB_PATH = ROOT_DIR / "resources" / "chroma_db"
 
 class BaseSystem(ABC):
 
@@ -60,15 +65,23 @@ class Retriever():
                 reranker_model: str="cross-encoder/ms-marco-MiniLM-L-6-v2"):
         
         self.local_embeddings = embeddings
-        self.vectorstore = Chroma.from_texts(texts=articles["texts"], embedding=self.local_embeddings, ids=articles["ids"], metadatas=articles["metadatas"])
-        self.config = self._load_config(config_path)
+        self.config = load_config(config_path)
+        self.vectorstore = self._load_vectorstore(articles)
         self.reranker = Reranker(reranker_model)
 
 
-    def _load_config(self, config_path: PathLike) -> dict:
-        with open(config_path, 'r') as ys:
-            config = yaml.safe_load(ys)
-        return config
+    def _load_vectorstore(self, articles):
+        if CHROMA_DB_PATH.exists() and any(CHROMA_DB_PATH.iterdir()):
+            print("🔁 Loading existing Chroma vectorstore...")
+            vectorstore = Chroma(
+                persist_directory=str(CHROMA_DB_PATH),
+                embedding_function=self.local_embeddings
+            )
+        else:
+            print("🆕 Building new Chroma vectorstore...")
+            vectorstore = Chroma.from_texts(texts=articles["texts"], embedding=self.local_embeddings, ids=articles["ids"], metadatas=articles["metadatas"], persist_directory=str(CHROMA_DB_PATH))
+        
+        return vectorstore
     
 
     def retrieve_documents(self, query: str, rerank: bool=True):
@@ -80,24 +93,46 @@ class Retriever():
 
 class RAG(BaseSystem):
 
-    def __init__(self, LLM: ChatOllama, retriever: Retriever, database: nx.MultiDiGraph, lt_memory):
+    def __init__(self, config_path: PathLike, LLM: ChatOllama, retriever: Retriever, database: nx.MultiDiGraph, lt_memory: Optional[str]):
+        self.config = load_config(config_path)
         self.LLM = LLM
         self.retriever = retriever
         self.database = database
-        self.system_prompt = SystemMessage(content="You are a helpful assistant that answers based only on the provided context. If the answer is not in the context, say 'I don't know.'")
+        self.system_prompt = SystemMessage(content=self.config["system_prompt"])
         self.st_memory = [self.system_prompt]
         self.lt_memory = lt_memory
 
 
     def run(self, input: str, rerank: bool=True) -> str:
         documents = self.retriever.retrieve_documents(input, rerank=rerank)
-        context = self._format_context(input, documents)
-        self.st_memory.append(input)
-        response = self.LLM.invoke(self.st_memory)
-        self.st_memory.append(response)
-        return self.LLM(context)
+        retrieved_context = self._get_text_from_documents(documents)
+        self.st_memory.append(HumanMessage(f"Context: {retrieved_context}\n\n{input}"))
+
+        response = ""
+        for chunk in self.LLM.stream(self.st_memory):
+            print(chunk.content, end='', flush=True)
+            response += chunk.content
+        self.st_memory.append(AIMessage(response))
+
+        return response
     
     
-    def _format_context(self, input: str, documents: list[Document]):
-        #ici formatter le contexte selon un template? CoT
+    def generate_question(self) -> str:
         pass
+
+    def _expand_context(self):
+        #navigate in the graph
+        pass
+
+    def _get_text_from_documents(self, documents: list[Document]) -> str:
+        content = []
+        for doc in documents:
+            origin = []
+            origin.append(self.database.nodes[doc.id]["data"].metadata["book"])
+            origin.append(self.database.nodes[doc.id]["data"].metadata["part"])
+            origin.append(self.database.nodes[doc.id]["data"].metadata["chapter"])
+            origin = " -> ".join(origin)
+            content.append(": ".join([origin, doc.page_content]))
+        retrieved_context = "\n".join(content)
+
+        return retrieved_context
--- src/utils/tools.py
+++ src/utils/tools.py
@@ -1,5 +1,12 @@
 import re
+from os import PathLike
+import yaml
 
 def get_base_id(id: str) -> str:
     match = re.match(r'\b(?:Article|Art\.|Rule|R\.)\s\d+[a-zA-Z]?\b', id)
     return match.group()
+
+def load_config(config_path: PathLike) -> dict:
+    with open(config_path, 'r') as ys:
+        config = yaml.safe_load(ys)
+    return config
